3b2cc535609a72b8db13b702d07dcd0b50c9c54f
IgorBrizkun <lyon90@mail.ru>
2016-02-21 22:09:15 +0300

    полный рефакторинг структура разбор SASS на модули добавил картинки оптимизация селекторов и модификаторо переход к методологии OPORCSS сделан слайдер услуги описание социалки перенес все в отдельные файлы все модули вынес удалил заккоментирование финальная подключил js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Igor on 21.02.2016.
+ */
+var link = document.querySelector(".mapContactBtn");
+var popup = document.querySelector(".modal-content");
+var close = popup.querySelector(".modal-content-close");
+link.addEventListener("click", function (event) {
+  event.preventDefault();
+  popup.classList.add("modal-content-show");
+});
+close.addEventListener("click", function (event) {
+  event.preventDefault();
+  popup.classList.remove("modal-content-show");
+});
